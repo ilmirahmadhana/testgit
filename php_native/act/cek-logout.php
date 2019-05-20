475fb79d2b60c03d41bba2810939b2e2034a843d
dieasylol1 <izzuddindwiiswantiarno@gmail.com>
2019-05-20 13:57:02 +0700

    kesalahan step saat push

--- php_native/act/cek-logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['username']);
-    session_destroy();
-    $message = "Kembali ke halaman login.";
-    echo "<script type='text/javascript'>alert('$message');
-          window.location.href='../index.php'</script>";
-?>
--- php_native/act/cek-session.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $message = "Anda harus login terlebih dahulu!\\nKembali ke halaman login.";
-    echo "<script type='text/javascript'>alert('$message');
-    window.location.href='index.php'</script>";
-?>
