627cbd89955c97d7ffd63b367c72e5e057d92a48
dieasylol1 <izzuddindwiiswantiarno@gmail.com>
2019-05-23 11:31:56 +0700

    perbaiki model lagi

--- application/models/m_bupe.php
+++ application/models/m_bupe.php
@@ -4,11 +4,6 @@ class M_bupe extends CI_Model{
 	function cek_login($table,$where){		
 		return $this->db->get_where($table,$where);
 	}	
-	
-}
-=======
-	}
-    
     function ambil_data_produk(){
         $query = $this->db->query("SELECT * FROM produk");
 		//return $query->result();
